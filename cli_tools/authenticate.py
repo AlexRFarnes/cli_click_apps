f238802a06f5da02041a9d37f56af83929868178
AlexRFarnes <alexrfarnes@gmail.com>
2025-07-23 13:13:55 +0800

    prompts

--- cli_tools/authenticate.py
+++ cli_tools/authenticate.py
@@ -4,6 +4,13 @@ import click
 
 
 @click.command()
-@click.option()
 def auth():
-    pass
+    """Provides user authentication"""
+    username = click.prompt(text="Username")
+    password = click.prompt(text="Password", hide_input=True, confirmation_prompt=True)
+
+    if click.confirm(text="Are you an admin?"):
+        admin_id = click.prompt(text="Admin ID", type=int, prompt_suffix=">")
+        click.echo(f"Loggin in admin {username} (ID={admin_id})")
+    else:
+        click.echo(f"Logging in {username}")
